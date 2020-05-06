f148a101d163e757b06fc5e913b6b48c87f11f09
Aaron-9503 <jay_web@163.com>
2020-05-06 21:32:51 +0800

    通过函数过滤数组中的重复元素

---
+++ a.js
@@ -0,0 +1,11 @@
+function getSame(arr) {
+    if (typeof arr !== 'object') {
+        return false
+    }
+    return arr.filter((n, index) => {
+        // console.log(arr.indexOf(n));
+        return arr.indexOf(n) === index
+    })
+}
+
+console.log(getSame([1,2,3,4,5,6,7,3,2,2,2,4,5,3]));
